462a73750d8db996ea61199fcc7ae35ce9a2077e
Thom <thomas.haussener@gmail.com>
2020-10-21 13:05:42 +0200

    Initial commit
    Initial commit

---
+++ example/element.js
@@ -0,0 +1,8 @@
+import '../src/x-364985-paypal-ordercheck';
+
+const el = document.createElement('DIV');
+document.body.appendChild(el);
+
+el.innerHTML = `		
+<x-364985-paypal-ordercheck></x-364985-paypal-ordercheck>
+`;
